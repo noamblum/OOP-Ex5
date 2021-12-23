a36336b500ab378487d30331a9332ff4b3c6f82d
Noam Blum <noam@d-bur.com>
2021-12-23 15:51:37 +0200

    Initial commit

---
+++ src/pepse/PepseGameManager.java
@@ -0,0 +1,27 @@
+package pepse;
+
+import danogl.GameManager;
+import danogl.gui.ImageReader;
+import danogl.gui.SoundReader;
+import danogl.gui.UserInputListener;
+import danogl.gui.WindowController;
+import danogl.util.Vector2;
+
+public class PepseGameManager extends GameManager {
+
+    public PepseGameManager(String windowTitle, Vector2 windowDimensions) {
+        super(windowTitle, windowDimensions);
+    }
+
+    @Override
+    public void initializeGame(ImageReader imageReader,
+                               SoundReader soundReader,
+                               UserInputListener inputListener,
+                               WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+    }
+
+    public static void main(String[] args) {
+        new PepseGameManager("PEPSE", new Vector2(700,500)).run();
+    }
+}
