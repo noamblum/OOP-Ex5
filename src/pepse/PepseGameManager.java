0d715dce668a50a7d572e2018c96d09aa9cc4a7e
Noam Blum <noam@d-bur.com>
2021-12-26 11:30:46 +0200

    Added basic sky

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -1,11 +1,13 @@
 package pepse;
 
 import danogl.GameManager;
+import danogl.collisions.Layer;
 import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
 import danogl.util.Vector2;
+import pepse.world.Sky;
 
 public class PepseGameManager extends GameManager {
 
@@ -19,6 +21,7 @@ public class PepseGameManager extends GameManager {
                                UserInputListener inputListener,
                                WindowController windowController) {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        Sky.create(gameObjects(), windowController.getWindowDimensions(), Layer.BACKGROUND);
     }
 
     public static void main(String[] args) {
---
+++ src/pepse/world/Sky.java
@@ -0,0 +1,36 @@
+package pepse.world;
+
+import danogl.GameObject;
+import danogl.components.CoordinateSpace;
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.util.Vector2;
+
+import java.awt.*;
+
+/**
+ * A class handling the world's sky
+ */
+public class Sky {
+
+    public static final Color SKY_COLOR = Color.decode("#80C6E5");
+    public static final String SKY_TAG = "SKY";
+
+    /**
+     * Create the background sky
+     * @param gameObjects The main game object collection
+     * @param windowDimensions The window's dimensions
+     * @param skyLayer The layer in which to add the sky
+     * @return A new GameObject representing the sky
+     */
+    public static danogl.GameObject create(
+            danogl.collisions.GameObjectCollection gameObjects,
+            danogl.util.Vector2 windowDimensions,
+            int skyLayer) {
+        GameObject sky = new GameObject(Vector2.ZERO, windowDimensions, new RectangleRenderable(SKY_COLOR));
+        sky.setTag(SKY_TAG);
+        sky.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
+        gameObjects.addGameObject(sky, skyLayer);
+        return sky;
+    }
+
+}
